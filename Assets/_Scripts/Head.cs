8f3430f9ea1bd4280e49d178b7d63abae0bd46cb
Gurujimaan <gabrugurmaan@gmail.com>
2026-07-18 21:34:16 +0530

    Head bounce bug fix

--- Assets/_Scripts/Head.cs
+++ Assets/_Scripts/Head.cs
@@ -3,23 +3,22 @@ using UnityEngine;
 public class Head : MonoBehaviour
 {
     [Header("References")]
-    public PlayerController playerController;
+    public Spring spring;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Floor"))
+        if (collision.gameObject.layer == 7)
         {
             Collider myColliderThatHit = collision.contacts[0].thisCollider;
-            if (myColliderThatHit.gameObject.name != "Head") return;
 
             ContactPoint contact = collision.contacts[0];
             Vector3 bounceDirection = contact.normal;
             bounceDirection.z = 0f;
             bounceDirection.Normalize();
 
-            playerController.rb.linearVelocity = Vector3.zero;
-            playerController.rb.angularVelocity = Vector3.zero;
-            playerController.rb.AddForce(bounceDirection * 3, ForceMode.Impulse);
+            spring.rb.linearVelocity = Vector3.zero;
+            spring.rb.angularVelocity = Vector3.zero;
+            spring.rb.AddForce(bounceDirection * 3, ForceMode.Impulse);
         }
     }
 }
